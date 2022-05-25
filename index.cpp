5ab84dbd8d37d26af98fa974c8598b142b7297dd
Duvier Reinosa <duvierreinosa@gmail.com>
2022-05-24 20:35:42 -0500

    agregué estructura

---
+++ index.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void menu() {
+	printf("1. Agregar Libro");
+	printf("2. Editar libro");
+	printf("3. Eliminar libro");
+	printf("4. Buscar/imprimir libro");
+	printf("5. vender libro");
+	printf("6. Editar venta");
+	printf("7. Eliminar Venta");
+	printf("8. Imprimir factura");
+	printf("9. Salir");
+}
+
+int main() {
+	
+}
